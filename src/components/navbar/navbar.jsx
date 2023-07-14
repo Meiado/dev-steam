5371719ae39605a43ec3e7616dcb2836aec52ee2
Meiado <gabrielmeiado@hotmail.com>
2023-07-14 10:59:35 -0300

    feat: add input

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import styles from './navbar.module.css'
 import { BsCart4 } from 'react-icons/bs'
 import { Logo } from '../logo/logo'
+import { Input } from '../forms/input/input'
 
 export const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <Logo />
-      <input />
+      <div className={styles.search}>
+        <Input placeHolder={"Buscar"} fullWidth />
+      </div>
       <BsCart4 size={40} />
     </div>
   )
