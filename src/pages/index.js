e266a2c676007f1004f2983af7ccc5fb982cdb25
Meiado <gabrielmeiado@hotmail.com>
2023-07-14 10:23:14 -0300

    feat: add salecard

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { Navbar } from '@/components/navbar/navbar'
 import { Subtitle } from '@/components/tipography/subtitle/subtitle'
 import { Container } from '@/components/container/container'
+import { SaleCard } from '@/components/cards/saleCard/saleCard'
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Container>
           <div>
             <Subtitle>Promoções</Subtitle>
+            <SaleCard img={"league-of-legends"} />
           </div>
           <div>
             <Subtitle>Outros jogos</Subtitle>
